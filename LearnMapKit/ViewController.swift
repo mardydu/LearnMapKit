89cdda3be497bb4206186e82dc00565a3849b2a5
Marcel Wira <marcelwira20@gmail.com>
2019-06-27 17:17:11 +0700

    Initial Commit

---
+++ LearnMapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnMapKit
+//
+//  Created by Marcel W on 27/06/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
